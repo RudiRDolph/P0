d5603149c1c52e357691036f1d1c87eae802c027
Rudi <rudi.r.dolph@gmail.com>
2021-08-18 03:28:59 +0300

    added "IF" in FOR. Unworkable

--- scriptMovie.js
+++ scriptMovie.js
@@ -21,7 +21,14 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     const a = prompt ('Один из просмотренных фильмов', ''),
           b = prompt ('Оценка', '');
-    personalMovieDB.movies[a] = b;
+
+    if (a != null && b != null && a != '' && b != '' && a.lenght < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log ('done!');
+    } else {
+        console.log ('error!');
+        i--;
+    }
 }
 
 console.log(personalMovieDB);
